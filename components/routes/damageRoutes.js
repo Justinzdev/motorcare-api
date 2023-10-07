8a7b634bc458132300a259b523c6547e724c97c6
Justinzdev <fullyz1532@gmail.com>
2023-10-08 02:26:20 +0700

    update

--- components/routes/damageRoutes.js
+++ components/routes/damageRoutes.js
@@ -4,5 +4,6 @@ const damageControllers = require('../controllers/damageControllers')
 damageRoutes.post('/add', damageControllers.addDamage)
 damageRoutes.patch('/cancel/:dm_id/:bp_id', damageControllers.cancelJob)
 damageRoutes.patch('/confirm/:dm_id/:bp_id', damageControllers.confirmJob)
+damageRoutes.patch('/finish/:dm_id/:bp_id', damageControllers.finishJob)
 
 module.exports = damageRoutes
